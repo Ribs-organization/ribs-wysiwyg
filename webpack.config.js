80c74217f84f12306f11f4ad662791cd9b6deb30
Piou-piou <pilloud.anthony@gmail.com>
2019-11-12 20:52:51 +0100

    [func] add base files of project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'js/index': './source/js/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'source/'),
+          path.join(__dirname, 'node_modules/ribs-core/'),
+        ],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.min.css',
+      chunkFilename:'css/style.min.css',
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+};
