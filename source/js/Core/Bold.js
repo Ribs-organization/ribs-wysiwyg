51989f59b2356adb2cc266281fbed3d473ff7e0c
Piou-piou <pilloud.anthony@gmail.com>
2019-11-13 21:03:37 +0100

    [edit] Bold class and method to initialize it

--- source/js/Core/Bold.js
+++ source/js/Core/Bold.js
@@ -1,3 +1,35 @@
-class Bold {
+import RibsWysiwygUtils from "../RibsWysiwygUtils";
 
+class Bold {
+  /**
+   * method to initialize Bold plugin
+   * @param toolbarDiv
+   */
+  constructor(toolbarDiv) {
+    this.toolbarDiv = toolbarDiv;
+    this.addBoldButtonToToolbar();
+  }
+
+  /**
+   * method to add bold button to toolbar
+   */
+  addBoldButtonToToolbar() {
+    const boldMenu = document.createElement('button');
+    boldMenu.id = 'ribs-wysiwyg-toolbar-bold';
+    boldMenu.textContent = 'B';
+    boldMenu.addEventListener('click', (event) => {
+      console.log(RibsWysiwygUtils.getSelectionText());
+    });
+    this.toolbarDiv.append(boldMenu);
+  }
 }
+
+/**
+ * function to load Bold class
+ * @param toolbarDiv
+ */
+function launchClass(toolbarDiv) {
+  const bold = new Bold(toolbarDiv);
+}
+
+module.exports = {launchClass};
