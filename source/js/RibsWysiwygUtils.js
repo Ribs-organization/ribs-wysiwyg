7048da1c639d5ec76b932c4e313ed0ee195624dc
Piou-piou <pilloud.anthony@gmail.com>
2019-12-10 08:02:29 +0100

    [func] add methods to save and restore selection

--- source/js/RibsWysiwygUtils.js
+++ source/js/RibsWysiwygUtils.js
@@ -41,6 +41,44 @@ class RibsWysiwygUtils {
     const caretLocationDiv = document.getElementById('ribs-wysiwyg-caret-location');
     caretLocationDiv.innerHTML = RibsWysiwygUtils.getCaretPositionAsString();
   }
+
+  /**
+   * save current selection of contentn editable
+   * @returns {null|[]|Range}
+   */
+  static saveSelection() {
+    if (window.getSelection) {
+      const selection = window.getSelection();
+      if (selection.getRangeAt && selection.rangeCount) {
+        const ranges = [];
+        for (let i = 0, length = selection.rangeCount; i < length; ++i) {
+          ranges.push(selection.getRangeAt(i));
+        }
+        return ranges;
+      }
+    } else if (document.selection && document.selection.createRange) {
+      return document.selection.createRange();
+    }
+    return null;
+  }
+
+  /**
+   * restore a given selection
+   * @param currentSelection
+   */
+  static restoreSelection(currentSelection) {
+    if (currentSelection) {
+      if (window.getSelection) {
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        for (let i = 0, length = currentSelection.length; i < length; ++i) {
+          selection.addRange(currentSelection[i]);
+        }
+      } else if (document.selection && currentSelection.select) {
+        currentSelection.select();
+      }
+    }
+  }
 }
 
 export default RibsWysiwygUtils;
